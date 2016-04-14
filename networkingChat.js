2b5794cb56fe42bf2c0e8b53a42fd5a47dd4077c
Tobias Gillström <Tobias Gillstrm>
2016-04-14 23:09:20 +0200

    added a chat to the game, it uses a diffrent namespace on socket.io then the game

--- networkingChat.js
+++ networkingChat.js
@@ -1,10 +1,12 @@
 module.exports = function (io) {
 
+    
+    
     io.on('connection',function (socket) {
         console.log('Connected to chat');
 
         socket.on('chat message',function (message){
-            io.emit('chat message',message);
+            io.emit('chat message',message + ' from: ' + socket.id);
         });
 
         socket.on('disconnect',function(){
--- public/assets/js/chat/chat.js
+++ public/assets/js/chat/chat.js
@@ -2,3 +2,12 @@
  * Created by Tobias on 2016-04-13.
  */
 
+function myFunction(){
+    socketChat.emit('chat message', $('#m').val());
+    $('#m').val('');
+    return false;
+}
+
+socketChat.on('chat message', function(msg){
+    $('#messages').append($('<li>').text(msg));
+});
